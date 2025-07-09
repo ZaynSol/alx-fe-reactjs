cc49febe6de4edad565456436bc55b9766917507
ZaynSol <zaynsolomonmar@gmail.com>
2025-07-09 16:11:00 +0300

    Task 3: Add UserProfile component with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'; // ✅ NEW
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -15,8 +16,8 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> {/* ✅ NEW */}
       <Footer />
-      
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
