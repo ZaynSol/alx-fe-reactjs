4ae19acd9755c34ee92ab1e04e943b7fe5c17002
ZaynSol <zaynsolomonmar@gmail.com>
2025-07-20 11:01:02 +0300

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,16 @@
-// src/components/UserProfile.jsx
+import { useContext } from 'react';
+import UserContext from '../UserContext';
 
-const UserProfile = (props) => {
+function UserProfile() {
+  const user = useContext(UserContext);
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>User Profile</h2>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
